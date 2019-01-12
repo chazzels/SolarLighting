c86988727b24f6bbbf135bc8e2e59528469ee9ab
chazzels <chazzels22@gmail.com>
2019-01-12 05:12:51 +0000

    putting basic class structure into Style Compositor module.

--- src/engine.engine/styleCompositor.ts
+++ src/engine.engine/styleCompositor.ts
@@ -3,3 +3,32 @@
 *	uses fixture attributes to layer a final composite style of all the highest 
 *	property values from home value (typically 0).
 */
+class StyleCompositor {
+	
+	constructor(options:any, perf:any) {
+		
+		this._pre_constructor();
+		
+		this._post_constructor();
+		
+	}
+	
+	
+	
+	private _pre_constructor() {
+		
+		console.log("COMPOSITOR::STARTING");
+		
+		console.group();
+		
+	}
+	
+	private _post_constructor() {
+		
+		console.groupEnd();
+		
+	}
+	
+}
+
+export = StyleCompositor;
