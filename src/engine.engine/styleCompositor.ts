78774e05344278949b96bda4f57340c6758ce988
chazzels <chazzels22@gmail.com>
2019-01-12 04:56:56 +0000

    adding a new engine module to composite styles together for a fixtures attributes. breif description left in file. No functional code at this time.

---
+++ src/engine.engine/styleCompositor.ts
@@ -0,0 +1,5 @@
+/*
+*	modue to composite a final style for a fixture to use.
+*	uses fixture attributes to layer a final composite style of all the highest 
+*	property values from home value (typically 0).
+*/
