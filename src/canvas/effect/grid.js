35529e5c58a42981a27995c29d2297038c462aa2
chazzels <chazzels22@gmail.com>
2019-12-06 21:31:03 +0000

    this work in the wrong file but it works. the ability to sample the enitre canvas now works. need to work on migrating this to its own file to be used by the engine.

--- src/canvas/effect/grid.js
+++ src/canvas/effect/grid.js
@@ -9,10 +9,13 @@ var Grid = function GridEffectConstructor(argContext, argColor) {
 	effect.assignContext(argContext);
 	
 	effect.makeParameter('color', '#000');
+	effect.makeParameter('colors', new Array());
 	effect.makeParameter('width', 100);
 	effect.makeParameter('height', 100);
 	effect.makeParameter('rows', 10);
 	effect.makeParameter('columns', 10);
+	
+	effect.makeParameter('profile', {});
 	
 	effect.makeHidden('xPos', new Int32Array());
 	effect.makeHidden('yPos', new Int32Array());
@@ -27,10 +30,34 @@ var Grid = function GridEffectConstructor(argContext, argColor) {
 	
 	function _effectDraw(ctx) {
 		
-		ctx.beginPath();
-		ctx.fillStyle = effect.parameter('color');
-		ctx.fillRect(0, 0, effect.parameter('width'), effect.parameter('height'));
-		ctx.closePath();
+		let hexColor = '#fff';
+		let tempColor = new Array(4);
+		
+		
+		for(var i = 0; i < effect.parameter('profile').length; i++) {
+			
+			tempColor = effect.parameter('colors')[i];
+			
+			hexColor = rgbToHex(
+				effect.parameter('colors')[i][0],
+				effect.parameter('colors')[i][1],
+				effect.parameter('colors')[i][2],
+				effect.parameter('colors')[i][3]
+			);
+			
+			ctx.beginPath();
+			
+			ctx.fillStyle = hexColor;
+			
+			ctx.fillRect(
+				effect.parameter('profile').xPos[i], 
+				effect.parameter('profile').yPos[i], 
+				effect.parameter('profile').width, 
+				effect.parameter('profile').height);
+			
+			ctx.closePath();
+			
+		}
 		
 	}
 	
@@ -58,11 +85,113 @@ var Grid = function GridEffectConstructor(argContext, argColor) {
 		
 	}
 	
-	function _loadColors() {
+	function _loadColors(colorArray) {
 		
+		effect.updateParameter('colors', colorArray);
+		
+	}
+	
+	function _createSampleProfile(context, argRows, argColumns, argWidth, argHeight) {
+		
+		let cWidth = context.canvas.width;
+		let cHeight = context.canvas.height;
+		
+		let widthGap = Math.floor(cWidth/argColumns) - argWidth;
+		let heightGap = Math.floor(cHeight/argRows) - argHeight;
+		
+		let xPos = new Int32Array(argRows * argColumns);
+		let yPos = new Int32Array(argRows * argColumns);
+		
+		let index = 0
+		for(var c = 0; c < argColumns; c++) {
+			for(var r = 0; r < argRows; r++) {
+				
+				index++;
+				
+				xPos[index] = Math.ceil(r*widthGap);
+				
+				yPos[index] = Math.ceil(c*heightGap);
+				
+			}
+		}
+		
+		var profileData = {
+			width: argWidth,
+			height: argHeight,
+			length: xPos.length,
+			xPos: xPos,
+			yPos: yPos
+		}
+		
+		effect.updateParameter('profile', profileData);
+		
+		return profileData;
 		
 		
 	}
+	
+	function _sampleAll(context, profile) {
+		
+		let colorSamples = new Array(profile.length);
+		
+		for(var i = 0; i < profile.length; i++) {
+			
+			let sample = _sampleCanvas(
+				context,
+				profile.xPos[i],
+				profile.yPos[i],
+				profile.width,
+				profile.height);
+			
+			colorSamples[i] = sample;
+			
+		}
+		
+		effect.updateParameter('colors', colorSamples);
+		
+		return colorSamples;
+		
+	}
+	
+	function _sampleCanvas(context, sx, sy, sw, sh) {
+		
+		let idata = context.getImageData(sx, sy, sw, sh);
+		
+		let maxColor = new Uint8ClampedArray(4);
+		
+		let curMax = 0;
+		
+		let tempMax = 0;
+		
+		for(var p = 0; p < Math.floor(idata.data.length/4); p++) {
+			
+			tempMax = idata.data[p*4]
+				+ idata.data[p*4+1]
+				+ idata.data[p*4+2]
+				+ idata.data[p*4+3];
+			
+			if(tempMax > curMax) {
+				
+				curMax = tempMax;
+				
+				maxColor[0] = idata.data[p*4];
+				maxColor[1] = idata.data[p*4+1];
+				maxColor[2] = idata.data[p*4+2];
+				maxColor[3] = idata.data[p*4+3];
+				
+			}
+			
+			
+		}
+		
+		return maxColor;
+		
+	}
+	
+	function rgbToHex(r, g, b) {
+		return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+	}
+	
 	
 	// binding functions to rendering system stages.
 	effect.setDraw(_effectDraw);
@@ -71,7 +200,9 @@ var Grid = function GridEffectConstructor(argContext, argColor) {
 	
 	var returnChainObject = {
 		renderAPI: effect.renderAPI,
-		updateParameter: effect.updateParameter
+		updateParameter: effect.updateParameter,
+		profile: _createSampleProfile,
+		sample: _sampleAll
 	}
 	
 	return returnChainObject;
